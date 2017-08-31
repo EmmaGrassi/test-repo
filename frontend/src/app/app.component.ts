13b516333edb6d9bfc32419435bb2c6e4efbee85
SrAxi <riccardo_polacci@hotmail.com>
2017-08-31 16:48:32 +0200

    Added an Api service Changed mock-data to a JSON file (in order to be called with api service) Added error handling and empty response handling

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
-import { MOCK_DATA } from '../assets/mock-data';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './shared/api/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   repositories: any;
 
-  constructor() {
-    this.repositories = MOCK_DATA;
+  // error handler
+  apiError = {
+    error: false,
+    msg: ''
+  };
+
+  constructor(private apiService: ApiService) {
+  }
+
+  ngOnInit() {
+    this.apiService.fetch('../../assets/mock-data.json').subscribe(
+      (repos) => {
+        this.repositories = repos;
+      },
+      (err) => {
+        this.apiError.error = true;
+        this.apiError.msg = err;
+      }
+    );
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,12 @@
+/*Core*/
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+/*Services*/
+import { ApiService } from './shared/api/api.service';
+
+/*Components*/
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
