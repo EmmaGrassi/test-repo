c2b3c652b663069bda55caa42680801e982ff202
SrAxi <riccardo_polacci@hotmail.com>
2017-08-31 16:56:18 +0200

    Added some comments Removed general RxJS import in the service, just importing what's needed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,9 +7,10 @@ import { ApiService } from './shared/api/api.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  /*The object that will contain our repositories*/
   repositories: any;
 
-  // error handler
+  // error handler object init
   apiError = {
     error: false,
     msg: ''
@@ -19,11 +20,14 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+    /*Calling the API backend to get the current repositories*/
     this.apiService.fetch('../../assets/mock-data.json').subscribe(
       (repos) => {
+        /*We received the data and will assign it. We don't care if it's empty or not, we're handling that in the template*/
         this.repositories = repos;
       },
       (err) => {
+        /*We set our error handler object in order to display useful information to the user in the template*/
         this.apiError.error = true;
         this.apiError.msg = err;
       }
