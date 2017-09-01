db7d32d04c69278fe069bf1936e9058d25f0831e
unknown <serg.firsov@gmail.com>
2017-09-01 16:11:22 +0200

    The Backend empty project added

---
+++ backend/src/main/java/com/sytac/caseapocalypse/CaseApocalypseApplication.java
@@ -0,0 +1,12 @@
+package com.sytac.caseapocalypse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseApocalypseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseApocalypseApplication.class, args);
+	}
+}
