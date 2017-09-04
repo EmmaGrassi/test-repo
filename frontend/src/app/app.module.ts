6ad940de995dd5e7ebc087c0ab6ee2933a81ca13
szabomarian <szabomarian@yahoo.com>
2017-09-04 15:57:37 +0200

    initial commit on google authentication

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 
 /*Services*/
 import { ApiService } from './shared/api/api.service';
+import { AuthenticationService } from './shared/api/authentication.service';
 
 /*Modules*/
 import { DevcasesModule } from './devcases/devcases.module';
@@ -12,18 +13,36 @@ import { LayoutModule } from './layout/layout.module';
 
 /*Components*/
 import { AppComponent } from './app.component';
+import { GoogleAuthenticationComponent } from './login/google-authentication/google-authentication.component';
+import {ClientConfig, GoogleApiModule, NG_GAPI_CONFIG} from 'ng-gapi';
+
+const GOOGLE_CLIENT_ID = '189868751709-oq1sgg1qkgj49h6pdk88uvfqiipmhlns.apps.googleusercontent.com';
+
+let gapiClientConfig: ClientConfig = {
+    clientId: GOOGLE_CLIENT_ID,
+    discoveryDocs: [],
+    //uncomment this later, after the changes in the console are propagated
+    ux_mode: 'redirect',
+    redirect_uri: 'http://localhost:4200/auth/google_oauth2/callback',
+    scope: [].join(' ')
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleAuthenticationComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     DevcasesModule,
-    LayoutModule
+    LayoutModule,
+    GoogleApiModule.forRoot({
+        provide: NG_GAPI_CONFIG,
+        useValue: gapiClientConfig
+    })
   ],
-  providers: [ApiService],
+  providers: [ApiService, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
