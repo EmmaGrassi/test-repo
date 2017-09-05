63af413e2a63cdd4d2d4f51925494d701ac4c377
SrAxi <riccardo_polacci@hotmail.com>
2017-09-05 10:34:38 +0200

    Access App if logged in

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,25 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthenticationService } from '../shared/api/authentication.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthenticationService) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-
-    // Instead of using localStorage we should check with Google APIs.
-    // And if user is logged in with Google we should check with the backend
-
-
-    if (localStorage.getItem('currentUser')) {
-      // logged in so return true
-      return true;
+    console.log('inside canActivate ' + this.authService.isUserSignedIn());
+    if (!this.authService.isUserSignedIn()) {
+      this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+      return false;
     }
-
-    // not logged in so redirect to login page with the return url
-    this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
-    return false;
+    return true;
   }
+
+  // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  //
+  //   // Instead of using localStorage we should check with Google APIs.
+  //   // And if user is logged in with Google we should check with the backend
+  //
+  //
+  //   if (sessionStorage.getItem('accessToken')) {
+  //     // logged in so return true
+  //     return true;
+  //   }
+  //
+  //   // not logged in so redirect to login page with the return url
+  //   this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+  //   return false;
+  // }
 }
